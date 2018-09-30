f83d619caccf846f537e9bd8d4003d4644a5abb3
Calvin J Foster <cal.j.foster@gmail.com>
2018-09-30 18:36:41 -0500

    new commit

---
+++ EmpathGoatApp/Duck.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpathGoatApp
+{
+    class Duck
+    {
+        string name { get; set; }
+
+        public event Action makeNoiseEvent;
+
+        public Duck()
+        {
+
+        }
+
+        public Duck(string _name)
+        {
+            name = _name;
+        }
+
+        public void Feeling()
+        {
+            Console.WriteLine(name + ": I felt a duck Quack, Quack!!!");
+        }
+
+        public void makeNoise()
+        {
+            Console.WriteLine("My name is " + name + ". Quack!!!");
+            makeNoiseEvent?.Invoke();
+        }
+
+        public void SubscribeToDuck(Duck foreignDuck)
+        {
+            foreignDuck.makeNoiseEvent += new System.Action(Feeling);
+        }
+
+
+    
+}
+}
--- EmpathGoatApp/Program.cs
+++ EmpathGoatApp/Program.cs
@@ -25,6 +25,15 @@ namespace EmpathGoatApp
             Jim.makeNoise();
             Mildred.makeNoise();
 
+            Duck James = new Duck("James");
+            James.makeNoise();
+            Duck Ricky = new Duck("Ricky");
+            Ricky.makeNoise();
+            Duck Eliza = new Duck("Eliza");
+            Eliza.SubscribeToDuck(James);
+            Duck Cheryl = new Duck("Cheryl");
+            Cheryl.SubscribeToDuck(Ricky);
+
 
             Console.ReadKey();
         }
